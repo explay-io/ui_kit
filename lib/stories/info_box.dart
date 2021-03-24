243c740ee86f01626a875ddde24b363422892558
Almas Akchabayev <almeynman@gmail.com>
2021-03-24 14:14:49 +0100

    change icon in info box

--- lib/stories/info_box.dart
+++ lib/stories/info_box.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:ui_kit/color.dart';
 import 'package:ui_kit/story_book/expandable_story.dart';
 import 'package:ui_kit/story_book/prop_updater/string_prop_updater.dart';
 import 'package:ui_kit/story_book/props_explorer.dart';
@@ -16,13 +18,9 @@ class InfoBoxes extends StatelessWidget {
 Widget _story() {
   return Container(
       child: SingleChildScrollView(
-          child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                _securedInfoBoxStory(),
-              ]
-          )
-      ));
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+    _securedInfoBoxStory(),
+  ])));
 }
 
 Widget _securedInfoBoxStory() {
@@ -30,9 +28,10 @@ Widget _securedInfoBoxStory() {
       title: 'Security Info Box',
       child: PropsExplorer(
           initialProps: const <String, dynamic>{
-            'assetName': 'assets/convert_is_secured.svg',
-            'infoHeader': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'infoBody': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin malesuada dui non egestas semper. Donec sit amet orci metus. Nam sit amet velit in ipsum lacinia suscipit. Duis quis erat convallis, vulputate neque nec, feugiat enim. Fusce laoreet, risus quis commodo tincidunt, nibh tortor dapibus neque, eget aliquet odio magna non leo. Cras viverra laoreet ante, a placerat purus fermentum sed. Cras lobortis blandit dolor, sed vestibulum purus faucibus id. Duis congue imperdiet erat consequat posuere. Quisque vel lorem odio. Nullam non diam ultrices, aliquet elit eget, viverra lorem. Donec vitae maximus lectus, eget pulvinar lacus. Nullam in tortor nisl. Aenean in blandit diam, nec cursus sem. Mauris eget rutrum nunc.'
+            'infoHeader':
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'infoBody':
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin malesuada dui non egestas semper. Donec sit amet orci metus. Nam sit amet velit in ipsum lacinia suscipit. Duis quis erat convallis, vulputate neque nec, feugiat enim. Fusce laoreet, risus quis commodo tincidunt, nibh tortor dapibus neque, eget aliquet odio magna non leo. Cras viverra laoreet ante, a placerat purus fermentum sed. Cras lobortis blandit dolor, sed vestibulum purus faucibus id. Duis congue imperdiet erat consequat posuere. Quisque vel lorem odio. Nullam non diam ultrices, aliquet elit eget, viverra lorem. Donec vitae maximus lectus, eget pulvinar lacus. Nullam in tortor nisl. Aenean in blandit diam, nec cursus sem. Mauris eget rutrum nunc.'
           },
           formBuilder: (context, props, updateProp) {
             return ListView(
@@ -55,10 +54,15 @@ Widget _securedInfoBoxStory() {
             return Column(mainAxisSize: MainAxisSize.min, children: [
               Container(
                   child: Center(
-                    child: SecurityInfoBox(
-                        props['assetName'],
-                        props['infoHeader'],
-                        props['infoBody']),
-                  ))]);
+                child: SecurityInfoBox(
+                  Icon(
+                    FontAwesome.ambulance,
+                    color: AppColor.blue,
+                  ),
+                  props['infoHeader'],
+                  props['infoBody'],
+                ),
+              ))
+            ]);
           }));
 }
