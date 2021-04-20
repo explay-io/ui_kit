333d051bfa247894a29d95f0bd5d54dcf5843d71
Adilkhan Kenzhetayev <adilkhankenzhetaev@gmail.com>
2021-04-20 21:04:36 +0600

    fix(wizards): remove nullable options

--- lib/wizards/wizard.dart
+++ lib/wizards/wizard.dart
@@ -5,13 +5,17 @@ import 'package:ui_kit/layouts/page_template.dart';
 import 'package:ui_kit/wizards/wizard_binder.dart';
 
 class Wizard extends StatefulWidget {
-  final WizardBinder? binder;
-  final Widget? intoWidget;
-  final List<Widget>? wizardPages;
-  final Widget? outroWidget;
+  final WizardBinder binder;
+  final Widget intoWidget;
+  final List<Widget> wizardPages;
+  final Widget outroWidget;
 
-  const Wizard(
-      {this.intoWidget, this.wizardPages, this.outroWidget, this.binder});
+  const Wizard({
+    required this.intoWidget,
+    this.wizardPages = const [],
+    required this.outroWidget,
+    required this.binder
+  });
 
   @override
   _WizardState createState() =>
@@ -19,12 +23,12 @@ class Wizard extends StatefulWidget {
 }
 
 class _WizardState extends State<Wizard> {
-  WizardBinder? binder;
+  WizardBinder binder;
   int currentPage = -1;
 
-  Widget? intoWidget;
-  List<Widget>? wizardPages;
-  Widget? outroWidget;
+  Widget intoWidget;
+  List<Widget> wizardPages;
+  Widget outroWidget;
 
   Widget? leadingWidget = const BackButton();
   List<Widget>? tailingWidget;
@@ -43,7 +47,7 @@ class _WizardState extends State<Wizard> {
         padding: const EdgeInsets.all(10.0),
         child: Column(
           children: <Widget>[
-            Expanded(flex: 1, child: _getCurrentPage()!),
+            Expanded(flex: 1, child: _getCurrentPage()),
             FilledButton(_callToActionText(), fullWidth: true,
                 onPressed: () async {
               setState(() {
@@ -59,20 +63,20 @@ class _WizardState extends State<Wizard> {
 
   void _callToActionEvent() {
     if (currentPage < 0) {
-      return binder!.onStart();
-    } else if (currentPage >= wizardPages!.length) {
-      return binder!.onEnd();
+      return binder.onStart();
+    } else if (currentPage >= wizardPages.length) {
+      return binder.onEnd();
     } else {
-      binder!.onNext(currentPage);
+      binder.onNext(currentPage);
     }
   }
 
   String _callToActionText() {
-    final text = binder!.getCallToAction(currentPage);
+    final text = binder.getCallToAction(currentPage);
     if (text == null) {
       if (currentPage < 0) {
         return 'Start Started';
-      } else if (currentPage >= wizardPages!.length) {
+      } else if (currentPage >= wizardPages.length) {
         return 'End';
       } else {
         return 'Next';
@@ -82,16 +86,16 @@ class _WizardState extends State<Wizard> {
     }
   }
 
-  Widget? _getCurrentPage() {
+  Widget _getCurrentPage() {
     if (currentPage < 0) {
       debugPrint('Showing intro wizard');
       return intoWidget;
-    } else if (currentPage >= wizardPages!.length) {
+    } else if (currentPage >= wizardPages.length) {
       debugPrint('Showing outro wizard');
       return outroWidget;
     } else {
       debugPrint('Showing wizard page[$currentPage]');
-      return wizardPages![currentPage];
+      return wizardPages[currentPage];
     }
   }
 }
